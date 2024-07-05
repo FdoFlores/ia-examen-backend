862a648959e2220b1e8b59c3a5ad2500a9dcebc2
FdoFlores <88992778+FdoFlores@users.noreply.github.com>
2024-07-04 22:17:33 -0700

    grimoire model

--- reino_del_trebol/api/models.py
+++ reino_del_trebol/api/models.py
@@ -3,6 +3,16 @@ from django.core.validators import MaxValueValidator, MinValueValidator
 from django.utils.crypto import get_random_string
 from .utils import validate_names
 
+class Grimoire(models.Model):
+    grimoire_name = models.CharField(max_length=100)
+    weight = models.DecimalField(decimal_places=2, max_digits=3)
+    
+    def __str__(self) -> str:
+        return f'{self.grimoire_name}'
+
+    class Meta:
+        db_table = 'grimoire'
+
 class MagicAffinity(models.Model):
     magic_name = models.CharField(max_length=255)
 
@@ -19,6 +29,7 @@ class Mage(models.Model):
     age = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(99)])
     status = models.BooleanField(default=False)
     affinity = models.ForeignKey(MagicAffinity, on_delete=models.CASCADE, related_name='affinities')
+    grimoire = models.ForeignKey(Grimoire, on_delete=models.CASCADE, related_name='grimoires', null=True, blank=True)
 
     def save(self, *args, **kwargs):
         if not self.id:
