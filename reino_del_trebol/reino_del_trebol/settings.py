b0f4514e9e2420411d36c1fdc65d527940c82ccb
FdoFlores <88992778+FdoFlores@users.noreply.github.com>
2024-07-05 17:32:31 -0700

    host fix

--- reino_del_trebol/reino_del_trebol/settings.py
+++ reino_del_trebol/reino_del_trebol/settings.py
@@ -85,6 +85,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'iaint_db',
         'USER': 'root',
+        'HOST': 'localhost',
         'PASSWORD': 'iaint123',
         'PORT': '5432',
     }
